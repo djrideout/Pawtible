84225920dbe09b3bacd20397581a6acc08ec4bd0
D.J. Rideout <d.j.rideout@mun.ca>
2019-08-26 21:33:24 -0230

    Start a MemoryMap class with onSet callback so I can create a view of memory

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello');
+import { MemoryMap } from "./nes/memory";
+
+let i = 0;
+let cb = () => i++;
+let map = new MemoryMap(cb);
+for(let i = 0x0000; i <= 0xFFFF; i++) {
+  map[i] = 0xBB;
+}
+map[0xCCCC]++;
+console.log(i.toString(16)); //0x10001
