18a70ba873508f21529707d52f78309cf8d6fd07
D.J. Rideout <d.j.rideout@mun.ca>
2019-08-27 23:04:37 -0230

    Started memory viewer

---
+++ src/view/react/canvas.jsx
@@ -0,0 +1,29 @@
+import { Screen } from "../screen";
+import * as React from "react";
+
+export class Canvas extends React.Component {
+  constructor(props) {
+    super(props);
+    this.canvas_ = null;
+    this.screen_ = null;
+  }
+
+  get GB() {
+    return this.props.gameBoy;
+  }
+
+  get Screen() {
+    return this.screen_;
+  }
+
+  componentDidMount() {
+    this.screen_ = new Screen(this.canvas_);
+    this.Screen.pinkScreen();
+  }
+
+  render() {
+    return (
+      <canvas id={"screen"} width={160} height={144} ref={ref => this.canvas_ = ref} />
+    );
+  }
+}
