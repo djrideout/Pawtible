6dcbe75329d69f2a8055c968846a33097fa5ed14
D.J. Rideout <djrideoutgit@proton.me>
2019-08-28 19:52:57 -0230

    Memory viewer complete

--- src/view/react/viewer.jsx
+++ src/view/react/viewer.jsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 
-let fuk = 1;
-
 export class GBViewer extends React.Component {
   constructor(props) {
     super(props);
+    this.setTop_ = set_top.bind(this);
+    this.setChanged_ = set_changed.bind(this);
+    this.onKeyPress_ = on_key_press.bind(this);
+    this.onScroll_ = on_scroll.bind(this);
+    this.scrollContainer_ = null;
     this.state = {
       top: 0x0000,
       changed: null
@@ -34,7 +37,7 @@ export class GBViewer extends React.Component {
     switch(true) {
       case inView(nextState):
       case inView(this.state) && !inView(nextState):
-      case nextState.top !== this.state.top:
+      case nextState.top !== this.top:
         return true;
       default:
         return false;
@@ -42,11 +45,7 @@ export class GBViewer extends React.Component {
   }
 
   componentDidMount() {
-    this.GB.M.addSetHook(addr => {
-      this.setState({
-        changed: addr
-      });
-    });
+    this.GB.M.addSetHook(this.setChanged_);
   }
 
   render() {
@@ -75,6 +74,45 @@ export class GBViewer extends React.Component {
       if(str2.length > 0) output.push(str2);
       output.push(<br key={`${i - this.top}-br`} />);
     }
-    return output;
+    return (
+      <>
+        <input id={"viewer-input"} type={"text"} onKeyPress={this.onKeyPress_}/>
+        <div id={"viewer-text"}>{output}</div>
+        <div id={"viewer-scroll-container"} onScroll={this.onScroll_} ref={ref => this.scrollContainer_ = ref}>
+          <div id={"viewer-scroll-space"} />
+        </div>
+      </>
+    );
   }
+}
+
+function set_top(row) {
+  this.setState({
+    top: row > ((this.GB.M.length - 1 >> 4) - this.rows) << 4 ? ((this.GB.M.length - 1 >> 4) - this.rows) << 4 : row
+  });
+}
+
+function set_changed(addr) {
+  this.setState({
+    changed: addr
+  });
+}
+
+function on_key_press(e) {
+  if(e.nativeEvent.keyCode === 13) { //enter
+    let addr = parseInt(e.target.value, 16);
+    if(Number.isInteger(addr) && addr <= this.GB.M.length - 1) {
+      let r = (addr >> 4 << 4) / ((this.GB.M.length - 1) >> 4 << 4);
+      let max = this.scrollContainer_.firstChild.offsetHeight - this.scrollContainer_.offsetHeight;
+      this.scrollContainer_.scrollTop = r * max;
+    }
+  }
+}
+
+function on_scroll(e) {
+  let el = e.target;
+  let max = el.firstChild.offsetHeight - el.offsetHeight;
+  let r = el.scrollTop / max;
+  let row = Math.ceil(((this.GB.M.length - 1) >> 4) * r) << 4;
+  this.setTop_(row);
 }
