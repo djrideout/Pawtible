ee5357b948d232b49c1151225a1f1b84b3427153
D.J. Rideout <djrideoutgit@proton.me>
2019-08-29 23:01:59 -0230

    Laying a little bit of groundwork for starting dev on the actual emulator

--- src/gb/index.js
+++ src/gb/index.js
@@ -1,9 +1,11 @@
-import { MemoryMap } from "./memory";
+import { Memory } from "./memory";
 import { CPU } from "./cpu";
+import { Cartridge } from "./cart";
 
 export class GameBoy {
   constructor() {
-    this.memoryMap_ = new MemoryMap();
+    this.cart_ = new Cartridge();
+    this.memoryMap_ = new Memory(this.cart_);
     this.cpu_ = new CPU();
   }
 
@@ -15,9 +17,17 @@ export class GameBoy {
     return this.cpu_;
   }
 
+  //Initial state from pan docs: http://bgb.bircd.org/pandocs.htm#cpuregistersandflags
+  //Initial I/O registers not done yet
   init() {
+    this.CPU.AF = 0x01B0;
+    this.CPU.BC = 0x0013;
+    this.CPU.DE = 0x00D8;
+    this.CPU.HL = 0x014D;
+    this.CPU.PC = 0x0100;
+    this.CPU.SP = 0xFFFE;
     for(let i = 0x0000; i <= 0xFFFF; i++) {
-      this.M.set(i, i);
+      this.M.set(i, 0x00);
     }
   }
 }
