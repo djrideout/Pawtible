e687184a98a756851099ea62dc7cc7c97739fa71
D.J. Rideout <d.j.rideout@mun.ca>
2019-08-31 17:07:37 -0230

    Refactor CPU to reduce code duplication and remove view updating context from the CPU entirely

--- src/gb/cpu.js
+++ src/gb/cpu.js
@@ -1,18 +1,18 @@
 export const Registers = {
-  A: "A",
-  F: "F",
-  AF: "AF",
-  B: "B",
-  C: "C",
-  BC: "BC",
-  D: "D",
-  E: "E",
-  DE: "DE",
-  H: "H",
-  L: "L",
-  HL: "HL",
-  SP: "SP",
-  PC: "PC"
+  A: "regA",
+  F: "regF",
+  AF: "regA_regF",
+  B: "regB",
+  C: "regC",
+  BC: "regB_regC",
+  D: "regD",
+  E: "regE",
+  DE: "regD_regE",
+  H: "regH",
+  L: "regL",
+  HL: "regH_regL",
+  SP: "regSP",
+  PC: "regPC"
 };
 
 export const Flags = {
@@ -24,161 +24,177 @@ export const Flags = {
 
 export class CPU {
   constructor() {
-    this.setHooks_ = [];
-
-    //registers
-    this.a_ = 0x01;
-    this.f_ = 0x02;
-    this.b_ = 0x03;
-    this.c_ = 0x04;
-    this.d_ = 0x05;
-    this.e_ = 0x06;
-    this.h_ = 0x07;
-    this.l_ = 0x08;
-    this.sp_ = 0x1234;
-    this.pc_ = 0x1234;
+    this[Registers.A] = 0x01;
+    this[Registers.F] = 0x02;
+    this[Registers.B] = 0x03;
+    this[Registers.C] = 0x04;
+    this[Registers.D] = 0x05;
+    this[Registers.E] = 0x06;
+    this[Registers.H] = 0x07;
+    this[Registers.L] = 0x08;
+    this[Registers.SP] = 0x1234;
+    this[Registers.PC] = 0x1234;
   }
 
-  addSetHook(callback) {
-    this.setHooks_.push(callback);
+  get(register) {
+    switch(register) {
+      case Registers.A:
+      case Registers.F:
+      case Registers.B:
+      case Registers.C:
+      case Registers.D:
+      case Registers.E:
+      case Registers.H:
+      case Registers.L:
+      case Registers.SP:
+      case Registers.PC:
+        return this[register];
+      case Registers.AF:
+      case Registers.BC:
+      case Registers.DE:
+      case Registers.HL:
+        let split = register.split("_");
+        return (this[split[0]] << 8) + this[split[1]];
+    }
   }
 
-  callSetHooks(register, val, flags = get_flags_updated()) {
-    for(let i = 0; i < this.setHooks_.length; i++) {
-      this.setHooks_[i](register, val, flags);
+  set(register, val) {
+    switch(register) {
+      case Registers.A:
+      case Registers.F:
+      case Registers.B:
+      case Registers.C:
+      case Registers.D:
+      case Registers.E:
+      case Registers.H:
+      case Registers.L:
+        this[register] = val & 0xFF;
+        break;
+      case Registers.AF:
+      case Registers.BC:
+      case Registers.DE:
+      case Registers.HL:
+        let split = register.split("_");
+        this[split[0]] = (val >> 8) & 0xFF;
+        this[split[1]] = val & 0xFF;
+        break;
+      case Registers.SP:
+      case Registers.PC:
+        this[register] = val & 0xFFFF;
+        break;
     }
   }
 
   get A() {
-    return this.a_;
+    return this.get(Registers.A);
   }
 
   set A(val) {
-    this.a_ = val & 0xFF;
-    this.callSetHooks(Registers.A, this.A);
+    this.set(Registers.A, val);
   }
 
   get F() {
-    return this.f_;
+    return this.get(Registers.F);
   }
 
   set F(val) {
-    let og = this.f_;
-    this.f_ = val & 0xFF
-    this.callSetHooks(Registers.F, this.F, get_flags_updated(og, this.f_));
+    this.set(Registers.F, val);
   }
 
   get AF() {
-    return (this.A << 8) + this.F;
+    return this.get(Registers.AF);
   }
 
   set AF(val) {
-    this.A = val >> 8;
-    let og = this.F;
-    this.F = val;
-    this.callSetHooks(Registers.AF, this.AF, get_flags_updated(og, this.F));
+    this.set(Registers.AF, val);
   }
 
   get B() {
-    return this.b_;
+    return this.get(Registers.B);
   }
 
   set B(val) {
-    this.b_ = val & 0xFF;
-    this.callSetHooks(Registers.B, this.B);
+    this.set(Registers.B, val);
   }
 
   get C() {
-    return this.c_;
+    return this.get(Registers.C);
   }
 
   set C(val) {
-    this.c_ = val & 0xFF;
-    this.callSetHooks(Registers.C, this.C);
+    this.set(Registers.C, val);
   }
 
   get BC() {
-    return (this.B << 8) + this.C;
+    return this.get(Registers.BC);
   }
 
   set BC(val) {
-    this.B = val >> 8;
-    this.C = val;
-    this.callSetHooks(Registers.BC, this.BC);
+    this.set(Registers.BC, val);
   }
 
   get D() {
-    return this.d_;
+    return this.get(Registers.D);
   }
 
   set D(val) {
-    this.d_ = val & 0xFF;
-    this.callSetHooks(Registers.D, this.D);
+    this.set(Registers.D, val);
   }
 
   get E() {
-    return this.e_;
+    return this.get(Registers.E);
   }
 
   set E(val) {
-    this.e_ = val & 0xFF;
-    this.callSetHooks(Registers.E, this.E);
+    this.set(Registers.E, val);
   }
 
   get DE() {
-    return (this.D << 8) + this.E;
+    return this.get(Registers.DE);
   }
 
   set DE(val) {
-    this.D = val >> 8;
-    this.E = val;
-    this.callSetHooks(Registers.DE, this.DE);
+    this.set(Registers.DE, val);
   }
 
   get H() {
-    return this.h_;
+    return this.get(Registers.H);
   }
 
   set H(val) {
-    this.h_ = val & 0xFF;
-    this.callSetHooks(Registers.H, this.H);
+    this.set(Registers.H, val);
   }
 
   get L() {
-    return this.l_;
+    return this.get(Registers.L);
   }
 
   set L(val) {
-    this.l_ = val & 0xFF;
-    this.callSetHooks(Registers.L, this.L);
+    this.set(Registers.L, val);
   }
 
   get HL() {
-    return (this.H << 8) + this.L;
+    return this.get(Registers.HL);
   }
 
   set HL(val) {
-    this.H = val >> 8;
-    this.L = val;
-    this.callSetHooks(Registers.HL, this.HL);
+    this.set(Registers.HL, val);
   }
 
   get SP() {
-    return this.sp_;
+    return this.get(Registers.SP);
   }
 
   set SP(val) {
-    this.sp_ = val & 0xFFFF;
-    this.callSetHooks(Registers.SP, this.SP);
+    this.set(Registers.SP, val);
   }
 
   get PC() {
-    return this.pc_;
+    return this.get(Registers.PC);
   }
 
   set PC(val) {
-    this.pc_ = val & 0xFFFF;
-    this.callSetHooks(Registers.PC, this.PC);
+    this.set(Registers.PC, val);
   }
 
   get FlagZ() {
@@ -214,11 +230,20 @@ export class CPU {
   }
 }
 
-function get_flags_updated(oldF = 0x00, newF = 0x00) {
-  return {
-    [Flags.Z]: !!((oldF & 0x80) ^ (newF & 0x80)),
-    [Flags.N]: !!((oldF & 0x40) ^ (newF & 0x40)),
-    [Flags.H]: !!((oldF & 0x20) ^ (newF & 0x20)),
-    [Flags.C]: !!((oldF & 0x10) ^ (newF & 0x10))
-  };
+export function get_flags_updated(register, oldVal, newVal) {
+  if(register === Registers.F || register === Registers.AF) {
+    return {
+      [Flags.Z]: !!((oldVal & 0x80) ^ (newVal & 0x80)),
+      [Flags.N]: !!((oldVal & 0x40) ^ (newVal & 0x40)),
+      [Flags.H]: !!((oldVal & 0x20) ^ (newVal & 0x20)),
+      [Flags.C]: !!((oldVal & 0x10) ^ (newVal & 0x10))
+    };
+  } else {
+    return {
+      [Flags.Z]: false,
+      [Flags.N]: false,
+      [Flags.H]: false,
+      [Flags.C]: false
+    };
+  }
 }
