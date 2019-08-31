78ad2b5359beeca623c9531563e06d28ee4c2c75
Akiteru <akiteru@live.com>
2019-08-31 14:49:04 -0230

    More memory refactoring

--- src/gb/mem/block.js
+++ src/gb/mem/block.js
@@ -1,6 +1,6 @@
 export class MemoryBlock {
-  constructor(key, length) {
-    this.key_ = key;
+  constructor(start, length) {
+    this.start_ = start;
     this.length_ = length;
     this.mem_ = new Array(length);
     this.mem_.fill(0x00);
@@ -18,13 +18,17 @@ export class MemoryBlock {
     return this.key_;
   }
 
+  get start() {
+    return this.start_;
+  }
+
   get length() {
     return this.length_;
   }
 
   //This is only necessary for the Echo RAM.
-  echo(key, length) {
-    let echo = new MemoryBlock(key, length);
+  echo(start, length) {
+    let echo = new MemoryBlock(start, length);
     echo.mem_ = this.mem_;
     return echo;
   }
