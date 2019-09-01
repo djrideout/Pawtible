e9f1d3bca127da72a25bdbdc8e079496a3973068
D.J. Rideout <d.j.rideout@mun.ca>
2019-09-01 00:49:31 -0230

    Modify set function closure to be at the prototype level so I can create new MemoryBlock objects and still have them update the view, basics of loading test ROM from path

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
+import testrompath from "../public/rom/cpu_instrs.gb";
 import { GameBoy } from "./gb";
 import { Site } from "./view/react/site";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-let gameBoy = new GameBoy();
-let mount = document.querySelector("#mount");
-let site = React.createElement(Site, {
-  gameBoy
-});
-ReactDOM.render(site, mount);
-
-window.gb = gameBoy;
+let xhr = new XMLHttpRequest();
+xhr.open("GET", testrompath);
+xhr.responseType = "arraybuffer";
+xhr.onload = () => {
+  let gameBoy = new GameBoy();
+  let mount = document.querySelector("#mount");
+  let site = React.createElement(Site, {
+    gameBoy,
+    testROM: new Uint8Array(xhr.response)
+  });
+  ReactDOM.render(site, mount);
+  window.gb = gameBoy;
+}
+xhr.send(null);
