2b26a324fad345e99622b29aef5423524e60e7c1
Akiteru <akiteru@live.com>
2019-09-01 17:51:03 -0230

    Implement ROM bank switching in MBC1 for test rom compatibility

--- src/gb/mem/block/cart/mbc1.js
+++ src/gb/mem/block/cart/mbc1.js
@@ -1,5 +1,32 @@
 import { Cartridge } from ".";
 
+/**
+ * EXTRAM not implemented yet
+ */
 export class MBC1 extends Cartridge {
-  
+  constructor(byteArr) {
+    super(byteArr);
+    this.bankNum_ = 0x01; //7 bits, 01-7F
+  }
+
+  get(addr) {
+    super.get(addr);
+    if(addr <= 0x3FFF) {
+      return this.rom_[addr];
+    } else {
+      return this.rom_[addr + 0x4000 * (this.bankNum_ - 1)];
+    }
+  }
+
+  set(addr, val) {
+    super.set(addr, val);
+    if(addr >= 0x2000 && addr <= 0x3FFF) {
+      if(val === 0x00) {
+        val = 0x01;
+      }
+      this.bankNum_ = (this.bankNum_ >> 5 << 5) + (val & 0b11111);
+    } else if(addr >= 0x4000 && addr <= 0x5FFF) {
+      this.bankNum_ = (this.bankNum_ & 0b11111) + ((val & 0b11) << 5);
+    }
+  }
 }
