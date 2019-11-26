794cb11a09879208c70639bce10aa7b60b5260a1
D.J. Rideout <djrideoutgit@proton.me>
2019-11-25 22:00:48 -0330

    Add some homebrew ROMs, basics of loading multiple ROMs

--- src/index.js
+++ src/index.js
@@ -1,20 +1,67 @@
-import testrompath from "../public/rom/cant_sue_me/drac.gb";
+import cpu_instrs from "../public/rom/cpu_instrs/cpu_instrs.gb";
+import instr_timing from "../public/rom/instr_timing/instr_timing.gb";
+import mem_timing from "../public/rom/mem_timing-2/mem_timing.gb";
+import mona from "../public/rom/homebrew/mona.gb";
+import retroid from "../public/rom/homebrew/retroid.gb";
 import { GameBoy } from "./gb";
 import { Site } from "./view/react/site";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-let xhr = new XMLHttpRequest();
-xhr.open("GET", testrompath);
-xhr.responseType = "arraybuffer";
-xhr.onload = () => {
+let romData = [
+  {
+    name: "cpu_instrs",
+    path: cpu_instrs,
+    rom: null
+  },
+  {
+    name: "instr_timing",
+    path: instr_timing,
+    rom : null
+  },
+  {
+    name: "mem_timing",
+    path: mem_timing,
+    rom: null
+  },
+  {
+    name: "mona",
+    path: mona,
+    rom: null
+  },
+  {
+    name: "retroid",
+    path: retroid,
+    rom: null
+  }
+];
+let load = path => {
+  return new Promise(resolve => {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", path);
+    xhr.responseType = "arraybuffer";
+    xhr.onload = () => {
+      resolve(new Uint8Array(xhr.response));
+    }
+    xhr.send(null);
+  });
+};
+let proms = [];
+for(let i = 0; i < romData.length; i++) {
+  proms.push(load(romData[i].path));
+}
+Promise.all(proms).then(bins => {
+  for(let i = 0; i < bins.length; i++) {
+    romData[i].rom = bins[i];
+  }
+  return romData;
+}).then(roms => {
   let gameBoy = new GameBoy();
   let mount = document.querySelector("#mount");
   let site = React.createElement(Site, {
     gameBoy,
-    testROM: new Uint8Array(xhr.response)
+    testROM: roms[0].rom
   });
   ReactDOM.render(site, mount);
   window.gb = gameBoy;
-}
-xhr.send(null);
+});
