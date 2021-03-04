d3ea8c955e2bea02b8b01c9658a2e6a1b8560be2
Akiteru <akiteru@live.com>
2021-03-04 18:09:35 -0330

    Remove the rest of the one-off function calls in PPU

--- src/gb/ppu.js
+++ src/gb/ppu.js
@@ -70,18 +70,18 @@ export class PPU {
   }
 
   step(cycles) {
-    if(!this.LCDEnable) {
+    if (!this.LCDEnable) {
       return;
     }
-    while(cycles > 0) {
+    while (cycles > 0) {
       this.cycles_++;
       cycles--;
-      if(this.cycles_ >= ModeCycles[this.ScreenMode]) {
+      if (this.cycles_ >= ModeCycles[this.ScreenMode]) {
         this.cycles_ -= ModeCycles[this.ScreenMode];
         let mode = this.ScreenMode;
         if (mode === Modes.HBLANK) {
           this.Line = this.Reg[Registers.LY] + 1;
-          if(this.Reg[Registers.LY] === 144) {
+          if (this.Reg[Registers.LY] === 144) {
             this.ScreenMode = Modes.VBLANK;
             this.GB.CPU.FlagVBlankRequest = true;
           } else {
@@ -89,7 +89,7 @@ export class PPU {
           }
         } else if (mode === Modes.VBLANK) {
           this.Line = this.Reg[Registers.LY] + 1;
-          if(this.Reg[Registers.LY] == 154) {
+          if (this.Reg[Registers.LY] == 154) {
             this.ScreenMode = Modes.OAM;
             this.Line = 0;
             this.Buffer = this.pixels_;
@@ -97,14 +97,156 @@ export class PPU {
           }
         } else if (mode === Modes.OAM) {
           this.sprites_ = [];
-          if(this.SpriteEnable) {
-            this.getSprites_(); //up to 10 sprites to be displayed on this line
+          if (this.SpriteEnable) {
+            //Get sprites. Up to 10 sprites to be displayed on this line
+            for (let i = 0xFE00; i < 0xFE9F; i += 4) {
+              let sprite = this.GB.M.get(i, 4, false);
+              let flags = (sprite >>> (8 * 3)) & 0xFF;
+              let number = (sprite >>> (8 * 2)) & 0xFF;
+              let x = ((sprite >>> 8) & 0xFF) - 8; //0 is -8 on the plane
+              let y = (sprite & 0xFF) - 16; //0 is -16 on the plane
+              if (this.Reg[Registers.LY] >= y && this.Reg[Registers.LY] < y + this.spriteSize_) {
+                //Online insertion sort the sprites array in descending order of priority
+                let middle = false;
+                for (let j = 0; j < this.sprites_.length; j += 4) {
+                  if (x < this.sprites_[j + 2] || x === this.sprites_[j + 2] && number < this.sprites_[j + 1]) {
+                    middle = true;
+                    this.sprites_.splice(j, 0, flags, number, x, y);
+                    break;
+                  }
+                }
+                if (!middle) {
+                  this.sprites_.push(flags, number, x, y);
+                }
+              }
+              if (this.sprites_.length === 40) {
+                //Only 10 sprites can be displayed on a line, where each sprite has 4 bytes of data
+                break;
+              }
+            }
           }
           this.ScreenMode = Modes.VRAM;
         } else if (mode === Modes.VRAM) {
           //TODO: Make this cycle-accurate instead of rendering the entire line at the end of mode 3
-          if(this.Reg[Registers.LY] < 144) {
-            this.renderLine_();
+          if (this.Reg[Registers.LY] < 144) {
+            for (let x = 0; x < 160; x++) {
+              //Use OAM and the BG/window map to determine the final color to be displayed at this pixel
+              //Get the background/window color existing at this pixel
+
+              //BG COLOR START
+              let bgcolor = null;
+
+              if (!this.BGWindowEnable) {
+                bgcolor = 0;
+              }
+          
+              //Determine which map to use for this pixel and calculate the offset from that map
+              let start = null;
+              let offsetX = null;
+              let offsetY = null;
+              if (this.WindowEnable && x >= this.Reg[Registers.WX] - 7 && this.Reg[Registers.LY] >= this.Reg[Registers.WY]) {
+                start = this.WindowMapStart;
+                offsetX = x - (this.Reg[Registers.WX] - 7);
+                offsetY = this.Reg[Registers.LY] - this.Reg[Registers.WY];
+              } else {
+                start = this.BGMapStart;
+                //Wrap around BG map if positions overflow
+                offsetX = (x + this.Reg[Registers.SCX]) % 256;
+                offsetY = (this.Reg[Registers.LY] + this.Reg[Registers.SCY]) % 256;
+              }
+          
+              //Get the tile this pixel is located in relative to the map
+              let mapX = Math.floor(offsetX / 8);
+              let mapY = Math.floor(offsetY / 8);
+              let mapNum = 32 * mapY + mapX;
+          
+              //Get the starting address for the tile data using the current addressing mode
+              let tileNum = this.GB.M.get(0x8000 + start + mapNum, 1, false);
+              let tileStart = null;
+
+              //all addresses are relative to the start of VRAM
+              switch ((this.Reg[Registers.LCDC] >>> 4) & 0x01) {
+                case 0:
+                  tileStart = 0x1000 + (tileNum << 24 >> 24) * 16;
+                  break;
+                case 1:
+                  tileStart = tileNum * 16;
+                  break;
+              }
+
+              //Get the pixel relative to the start of the tile
+              let tileX = offsetX - mapX * 8;
+              let tileY = offsetY - mapY * 8;
+          
+              //Get the relevant tile data
+              let lineByte0 = this.GB.M.get(0x8000 + tileStart + tileY * 2, 1, false);
+              let lineByte1 = this.GB.M.get(0x8000 + tileStart + tileY * 2 + 1, 1, false);
+          
+              //Get the color for this pixel
+              let shift = 8 - tileX - 1;
+              let ms = (lineByte1 & (0x01 << shift)) >> shift;
+              let ls = (lineByte0 & (0x01 << shift)) >> shift;
+              bgcolor = (ms << 1) | ls;
+              //BG COLOR END
+
+              //Get the highest priority sprite with non-zero color at this pixel
+              let color = null;
+              let behind = null;
+              let palette = null;
+              for (let i = 0; i < this.sprites_.length; i += 4) {
+                if (x < this.sprites_[i + 2] || x >= this.sprites_[i + 2] + 8) {
+                  continue;
+                }
+                
+                //Unpack the flags for the sprite
+                behind = (this.sprites_[i] >>> 7) & 0x01;
+                palette = (this.sprites_[i] >>> 4) & 0x01;
+                let yFlip = (this.sprites_[i] >>> 6) & 0x01;
+                let xFlip = (this.sprites_[i] >>> 5) & 0x01;
+
+                //Get the tile start positions for the sprite
+                //(Tile numbers are always unsigned in sprites)
+                let start = this.sprites_[i + 1] * 16;
+
+                //Using the flipped state of the sprite, find the corresponding pixel in the sprite that will map to this x and y on the screen.
+                let tileX = null;
+                if (xFlip) {
+                  tileX = this.sprites_[i + 2] + 7 - x;
+                } else {
+                  tileX = x - this.sprites_[i + 2];
+                }
+                let tileY = null
+                if (yFlip) {
+                  tileY = this.sprites_[i + 3] + this.spriteSize_ - 1 - this.Reg[Registers.LY];
+                } else {
+                  tileY = this.Reg[Registers.LY] - this.sprites_[i + 3];
+                }
+
+                //Get the tile data for this line
+                let lineByte0 = this.GB.M.get(0x8000 + start + tileY * 2, 1, false);
+                let lineByte1 = this.GB.M.get(0x8000 + start + tileY * 2 + 1, 1, false);
+
+                //Get the color for this pixel
+                let shift = 8 - tileX - 1;
+                let ms = (lineByte1 & (0x01 << shift)) >> shift;
+                let ls = (lineByte0 & (0x01 << shift)) >> shift;
+                color = (ms << 1) | ls;
+
+                if (color !== 0) {
+                  break;
+                }
+              }
+
+              if (color === null || color === 0 || behind && bgcolor !== 0) {
+                this.pixels_.push(this.bgShades_[bgcolor]);
+              } else {
+                if (palette === 0) {
+                  this.pixels_.push(this.obp0Shades_[color]);
+                } else if (palette === 1) {
+                  this.pixels_.push(this.obp1Shades_[color]);
+                }
+              }
+            }
           }
           this.ScreenMode = Modes.HBLANK;
         }
@@ -126,30 +268,23 @@ export class PPU {
           break;
       }
       //STAT IRQ is triggered on rising edge of this internal signal
-      if(this.prev_ === false && bool === true) {
+      if (this.prev_ === false && bool === true) {
         this.GB.CPU.FlagLCDSTATRequest = true;
       }
       this.prev_ = bool;
       //STAT IRQ END
 
       //DMA START
-      if(this.dmaRemain_ > 0) {
+      if (this.dmaRemain_ > 0) {
         this.dmaRemain_--;
         this.dmaThreshold_--;
-        if(this.dmaThreshold_ === -1) {
+        if (this.dmaThreshold_ === -1) {
           this.dmaThreshold_ = 3;
           let byte = (DMA_TOTAL - this.dmaRemain_ - 5) / 4;
           this.GB.M.set(0xFE00 + byte, this.GB.M.get(this.DMASourceAddr + byte, 1, false), 1, false);
         }
       }
       //DMA END
-    }
-  }
-
-  renderLine_() {
-    for(let i = 0; i < 160; i++) {
-      let shade = this.PixelShade(i);
-      this.pixels_.push(shade);
     }
   }
 
@@ -352,164 +487,5 @@ export class PPU {
       case 1:
         return 0x1C00;
     }
-  }
-
-  BGTileStart(num) {
-    let offset = null;
-    //all addresses are relative to the start of VRAM
-    switch((this.Reg[Registers.LCDC] >>> 4) & 0x01) {
-      case 0:
-        offset = (num << 24 >> 24) * 16;
-        return 0x1000 + offset;
-      case 1:
-          offset = num * 16;
-          return offset;
-    }
-  }
-
-  SpriteTileStart(num) {
-    //Sprite tile numbers are all unsigned
-    offset = num * 16;
-    return offset;
-  }
-
-  getSprites_() {
-    for(let i = 0xFE00; i < 0xFE9F; i += 4) {
-      let sprite = this.GB.M.get(i, 4, false);
-      let flags = (sprite >>> (8 * 3)) & 0xFF;
-      let number = (sprite >>> (8 * 2)) & 0xFF;
-      let x = ((sprite >>> 8) & 0xFF) - 8; //0 is -8 on the plane
-      let y = (sprite & 0xFF) - 16; //0 is -16 on the plane
-      if(this.Reg[Registers.LY] >= y && this.Reg[Registers.LY] < y + this.spriteSize_) {
-        //Online insertion sort the sprites array in descending order of priority
-        let middle = false;
-        for(let j = 0; j < this.sprites_.length; j += 4) {
-          if(x < this.sprites_[j + 2] || x === this.sprites_[j + 2] && number < this.sprites_[j + 1]) {
-            middle = true;
-            this.sprites_.splice(j, 0, flags, number, x, y);
-            break;
-          }
-        }
-        if(!middle) {
-          this.sprites_.push(flags, number, x, y);
-        }
-      }
-      if(this.sprites_.length === 40) {
-        //Only 10 sprites can be displayed on a line, where each sprite has 4 bytes of data
-        return;
-      }
-    }
-  }
-
-  /**
-   * Use OAM and the BG/window map to determine the final color to be displayed at this pixel
-   */
-  PixelShade(x) {
-    //Get the background/window color existing at this pixel
-    let bgcolor = this.BGPixelColor(x);
-
-    //Get the highest priority sprite with non-zero color at this pixel
-    let color = null;
-    let behind = null;
-    let palette = null;
-    for(let i = 0; i < this.sprites_.length; i += 4) {
-      if(x < this.sprites_[i + 2] || x >= this.sprites_[i + 2] + 8) {
-        continue;
-      }
-      
-      //Unpack the flags for the sprite
-      behind = (this.sprites_[i] >>> 7) & 0x01;
-      palette = (this.sprites_[i] >>> 4) & 0x01;
-      let yFlip = (this.sprites_[i] >>> 6) & 0x01;
-      let xFlip = (this.sprites_[i] >>> 5) & 0x01;
-
-      //Get the tile start positions for the sprite
-      //(Tile numbers are always unsigned in sprites)
-      let start = this.sprites_[i + 1] * 16;
-
-      //Using the flipped state of the sprite, find the corresponding pixel in the sprite that will map to this x and y on the screen.
-      let tileX = null;
-      if(xFlip) {
-        tileX = this.sprites_[i + 2] + 7 - x;
-      } else {
-        tileX = x - this.sprites_[i + 2];
-      }
-      let tileY = null
-      if(yFlip) {
-        tileY = this.sprites_[i + 3] + this.spriteSize_ - 1 - this.Reg[Registers.LY];
-      } else {
-        tileY = this.Reg[Registers.LY] - this.sprites_[i + 3];
-      }
-
-      //Get the tile data for this line
-      let lineByte0 = this.GB.M.get(0x8000 + start + tileY * 2, 1, false);
-      let lineByte1 = this.GB.M.get(0x8000 + start + tileY * 2 + 1, 1, false);
-
-      //Get the color for this pixel
-      let shift = 8 - tileX - 1;
-      let ms = (lineByte1 & (0x01 << shift)) >> shift;
-      let ls = (lineByte0 & (0x01 << shift)) >> shift;
-      color = (ms << 1) | ls;
-
-      if(color !== 0) {
-        break;
-      }
-    }
-
-    if(color === null || color === 0 || behind && bgcolor !== 0) {
-      return this.bgShades_[bgcolor];
-    } else {
-      if(palette === 0) {
-        return this.obp0Shades_[color];
-      } else if(palette === 1) {
-        return this.obp1Shades_[color];
-      }
-    }
-  }
-
-  BGPixelColor(x) {
-    if(!this.BGWindowEnable) {
-      return 0;
-    }
-
-    //Determine which map to use for this pixel and calculate the offset from that map
-    let start = null;
-    let offsetX = null;
-    let offsetY = null;
-    if(this.WindowEnable && x >= this.Reg[Registers.WX] - 7 && this.Reg[Registers.LY] >= this.Reg[Registers.WY]) {
-      start = this.WindowMapStart;
-      offsetX = x - (this.Reg[Registers.WX] - 7);
-      offsetY = this.Reg[Registers.LY] - this.Reg[Registers.WY];
-    } else {
-      start = this.BGMapStart;
-      //Wrap around BG map if positions overflow
-      offsetX = (x + this.Reg[Registers.SCX]) % 256;
-      offsetY = (this.Reg[Registers.LY] + this.Reg[Registers.SCY]) % 256;
-    }
-
-    //Get the tile this pixel is located in relative to the map
-    let mapX = Math.floor(offsetX / 8);
-    let mapY = Math.floor(offsetY / 8);
-    let mapNum = 32 * mapY + mapX;
-
-    //Get the starting address for the tile data using the current addressing mode
-    let tileNum = this.GB.M.get(0x8000 + start + mapNum, 1, false);
-    let tileStart = this.BGTileStart(tileNum);
-
-    //Get the pixel relative to the start of the tile
-    let tileX = offsetX - mapX * 8;
-    let tileY = offsetY - mapY * 8;
-
-    //Get the relevant tile data
-    let lineByte0 = this.GB.M.get(0x8000 + tileStart + tileY * 2, 1, false);
-    let lineByte1 = this.GB.M.get(0x8000 + tileStart + tileY * 2 + 1, 1, false);
-
-    //Get the color for this pixel
-    let shift = 8 - tileX - 1;
-    let ms = (lineByte1 & (0x01 << shift)) >> shift;
-    let ls = (lineByte0 & (0x01 << shift)) >> shift;
-    let color = (ms << 1) | ls;
-
-    return color;
   }
 }
