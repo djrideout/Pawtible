f783e2bfdc0be0eaf0e0399707e123fd41ec007b
Akiteru <akiteru@live.com>
2021-07-22 20:20:55 -0230

    Restore old version of APU. Starting from scratch with the test ROMS.

--- src/gb/apu.js
+++ src/gb/apu.js
@@ -22,84 +22,22 @@ export const Registers = {
   NR52: 20  //FF26
 };
 
-const SquareDuties = [
-  0b00000001,
-  0b10000001,
-  0b10000111,
-  0b01111110
-];
-
-// https://nightshade256.github.io/2021/03/27/gb-sound-emulation.html
-// https://gbdev.io/pandocs/Sound_Controller.html
 export class APU {
   constructor(gameBoy) {
     this.GB = gameBoy;
     this.reset();
   }
 
-  reset() {
-    this.Reg = new Uint8Array(21);
-    this.queueTimer_ = 0;
-    this.frameSequencerClock_ = 0;
-    this.enabled_ = true;
-    this.Channel2 = {
-      enabled: true,
-      freq: 0,
-      freqTimer: 0,
-      length: 0,
-      lengthTimer: 0,
-      pos: 0,
-      restart: 0
-    };
-  }
-
   step(cycles) {
-    if (!this.enabled_) {
-      return;
-    }
-    while (cycles > 0) {
-      cycles--;
-      this.Channel2.freqTimer--;
-      if (this.Channel2.freqTimer <= 0) {
-        this.Channel2.freqTimer = (2048 - this.Channel2.freq) * 4;
-        this.Channel2.pos = (this.Channel2.pos + 1) % 8;
-      }
-      this.queueTimer_--;
-      if (this.queueTimer_ <= 0) {
-        this.queueTimer_ = 87; //approx. 4194304Hz/48000Hz
 
-      }
-    }
   }
 
   stepFrameSequencer() {
-    this.frameSequencerClock_ = (this.frameSequencerClock_ + 1) % 8;
-    if (this.frameSequencerClock_ & 0b1 === 0) {
-      this.stepLength_();
-    }
-    if (this.frameSequencerClock_ === 7) {
-      this.stepEnvelope_();
-    }
-    if (this.frameSequencerClock_ & 0b10 === 1 && this.frameSequencerClock_ & 0b1 === 0) {
-      this.stepSweep_();
-    }
-  }
-
-  stepLength_() {
-    if (this.Channel2LengthEnabled && this.Channel2.lengthTimer > 0) {
-      this.Channel2.lengthTimer--;
-      if (this.Channel2.lengthTimer === 0) {
-        this.Channel2.enabled = false;
-      }
-    }
-  }
-
-  stepEnvelope_() {
 
   }
 
-  stepSweep_() {
-
+  reset() {
+    this.Reg = new Uint8Array(21);
   }
 
   set NR10(val) {
@@ -123,9 +61,7 @@ export class APU {
   }
 
   set NR21(val) {
-    this.Channel2.length = val & 0b111111;
-    this.Channel2.lengthTimer = 64 - this.Channel2.length;
-    this.Reg[Registers.NR21] = val & ~0b111111;
+    this.Reg[Registers.NR21] = val;
   }
 
   set NR22(val) {
@@ -133,21 +69,11 @@ export class APU {
   }
 
   set NR23(val) {
-    this.Channel2.freq = (this.Channel2.freq &= ~0b11111111) | val;
+    this.Reg[Registers.NR23] = val;
   }
 
   set NR24(val) {
-    this.Channel2.freq = (this.Channel2.freq &= ~0b11100000000) | (val & 0b111 << 8);
-    this.Channel2.restart = this.Channel2.freq & 0b1000000;
-    if (this.Reg[Registers.NR24] & 0b10000000) {
-      // Trigger Event
-      this.Channel2.enabled = true;
-      if (this.Channel2.lengthTimer === 0) {
-        this.Channel2.lengthTimer = 64;
-      }
-      this.Channel2.freqTimer = (2048 - this.Channel2.freq) * 4;
-    }
-    this.Reg[Registers.NR24] = val & ~0b10000111;
+    this.Reg[Registers.NR24] = val;
   }
 
   set NR30(val) {
@@ -196,29 +122,5 @@ export class APU {
 
   set NR52(val) {
     this.Reg[Registers.NR52] = val;
-  }
-
-  get Channel2Amplitude() {
-    return SquareDuties[this.Channel2Duty][this.Channel2.pos];
-  }
-
-  get Channel2Duty() {
-    return this.Reg[Registers.NR21] & 0b11000000 >> 6;
-  }
-
-  get Channel2EnvelopeVolume() {
-    return this.Reg[Registers.NR22] & 0b11110000 >> 4;
-  }
-
-  get Channel2EnvelopeDirection() {
-    return this.Reg[Registers.NR22] & 0b1000 >> 3;
-  }
-
-  get Channel2EnvelopeSweep() {
-    return this.Reg[Registers.NR22] & 0b111;
-  }
-
-  get Channel2LengthEnabled() {
-    return this.Reg[Registers.NR24] & 0b1000000;
   }
 }
